dc6dee918523a3a5cc5a1931243ed72c5ab3f920
Ben Li <benji112358@gmail.com>
2020-03-09 20:53:12 -0400

    time-test (Nour)

--- Raspberry Pi/LaneDetection/RPilaneDetection_withTimeTest.py
+++ Raspberry Pi/LaneDetection/RPilaneDetection_withTimeTest.py
@@ -1,11 +1,3 @@
-'''
-SPEED TEST
----------
-- Dell XPS 13: 
-    - 20 fps with native webcam res 
-    - 18 fps with enforced 640x420 res
-- Raspberry Pi:  
-'''
 import numpy as np
 import logging
 import math
@@ -17,15 +9,12 @@ import time
 _SHOW_IMAGE = False
 _ENFORCE_RESOLUTION = True
 
-CAM_WIDTH = 160
-CAM_HEIGHT = 120
-NUM_FRAMES = 100    # process this many frames before quitting program
 
 class HandCodedLaneFollower(object):
     
     # CONSTRUCTOR
     def __init__(self, car=None):
-        logging.info('Creating a HandCodedLaneFollower...')
+        #logging.info('Creating a HandCodedLaneFollower...')
         self.car = car
         self.curr_steering_angle = 90
 
@@ -39,9 +28,9 @@ class HandCodedLaneFollower(object):
     
     # COMPUTES STEERING DIRECTION
     def steer(self, frame, lane_lines_arr):
-        logging.debug('steering...')
+        #logging.debug('steering...')
         if len(lane_lines_arr) == 0:
-            logging.error('No lane lines detected, nothing to do.')
+            #logging.error('No lane lines detected, nothing to do.')
             return frame
             
         new_steering_angle = compute_steering_angle(frame, lane_lines_arr)
@@ -62,7 +51,9 @@ class HandCodedLaneFollower(object):
 
 '''LANE-DETECTION MASTER'''
 def detect_lane(frame):
-    logging.debug('detecting lane lines...')
+
+    e1 = cv2.getTickCount()
+    #logging.debug('detecting lane lines...')
     # EDGE DETECTION
     edges = detect_edges(frame)
     show_image('edges', edges)
@@ -77,9 +68,16 @@ def detect_lane(frame):
     lane_lines_arr = average_slope_intercept(frame, line_segments_arr)
     lane_lines_image = display_lines(frame, lane_lines_arr)
     show_image("lane lines", lane_lines_image)
+
+    e2 = cv2.getTickCount()
+    time = (e2-e1)/cv2.getTickFrequency()
+    print("detect_lane: ", time)
+
     return lane_lines_arr, lane_lines_image
 
 def detect_edges(frame):
+
+    e1 = cv2.getTickCount()
     # only check pixels that're the color of lane-lines
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     show_image("hsv", hsv)
@@ -87,10 +85,15 @@ def detect_edges(frame):
     # detect the remaining edges in the image, including lane-lines
     edges = cv2.Canny(mask, 200, 400)
     show_image("blue mask", mask)
+    e2 = cv2.getTickCount()
+    time = (e2-e1)/cv2.getTickFrequency()
+    print("detect_edges: ", time)
     return edges
 
 # lane-lines are in bottom half of frame
 def region_of_interest(canny):
+
+    e1 = cv2.getTickCount()
     height, width = canny.shape
     mask = np.zeros_like(canny)
     # 4-corners of top-half
@@ -104,6 +107,11 @@ def region_of_interest(canny):
     cv2.fillPoly(mask, polygon, 255)
     masked_image = cv2.bitwise_and(canny, mask)
     show_image("mask", mask)
+
+    e2 = cv2.getTickCount()
+    time = (e2-e1)/cv2.getTickFrequency()
+    print("region_of_interest: ", time)
+
     return masked_image
 
 # find LINES in frame
@@ -112,20 +120,28 @@ def detect_line_segments(cropped_edges):
     No mask will cleanly identify the 2 lane-lines. 
     Instead, the mask detects a multiple lane-segments for each lane.
     '''
+
+    e1 = cv2.getTickCount()
     # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
     rho = 1  # precision in pixel, i.e. 1 pixel
     angle = np.pi / 180  # degree in radian, i.e. 1 degree
     min_threshold = 10  # minimal of votes
     line_segments_arr = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, np.array([]), minLineLength=8,
                                     maxLineGap=4)
-    if line_segments_arr is not None:
-        for line_segment in line_segments_arr:
-            logging.debug('detected line_segment:')
-            logging.debug("%s of length %s" % (line_segment, length_of_line_segment(line_segment[0])))
+    #if line_segments_arr is not None:
+     #   for line_segment in line_segments_arr:
+            #logging.debug('detected line_segment:')
+            #logging.debug("%s of length %s" % (line_segment, length_of_line_segment(line_segment[0])))
+
+    e2 = cv2.getTickCount()
+    time = (e2-e1)/cv2.getTickFrequency()
+    print("detect_line_segments: ", time)
     return line_segments_arr
 
 # derive LANE-LINES from identified line-segments
 def average_slope_intercept(frame, line_segments_arr):
+
+    e1 = cv2.getTickCount()
     """
     This function combines line-segments into one or two lane lines
     If all line slopes are < 0: then we only have detected left lane
@@ -133,7 +149,7 @@ def average_slope_intercept(frame, line_segments_arr):
     """
     lane_lines_arr = []
     if line_segments_arr is None:
-        logging.info('No line_segment segments detected')
+        #logging.info('No line_segment segments detected')
         return lane_lines_arr
 
     height, width, _ = frame.shape
@@ -145,14 +161,17 @@ def average_slope_intercept(frame, line_segments_arr):
     boundary = 1/3
     left_region_boundary = width * (1 - boundary)  # L lane-line is on left 2/3 of screen
     right_region_boundary = width * boundary       # R lane line is right 2/3 (the area outside this bound)
-
+    
     # use SLOPE to differentite L&R lane-segments
     for line_segment in line_segments_arr:
         for x1, y1, x2, y2 in line_segment:        
             if x1 == x2:
-                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                #logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
                 continue
+            ee1 = cv2.getTickCount()
             fit = np.polyfit((x1, x2), (y1, y2), 1)
+            ee2 = cv2.getTickCount()
+            # print("polyfit : ", (ee2-ee1)/cv2.getTickFrequency())
             slope = fit[0]
             intercept = fit[1]
             # left lane
@@ -173,7 +192,11 @@ def average_slope_intercept(frame, line_segments_arr):
     if len(right_fit_segments) > 0:
         lane_lines_arr.append(make_points(frame, right_fit_average))
 
-    logging.debug('lane lines: %s' % lane_lines_arr)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+    #logging.debug('lane lines: %s' % lane_lines_arr)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    e2 = cv2.getTickCount()
+    time = (e2 - e1)/cv2.getTickFrequency()
+    print("average_slope_intercept :", time)
 
     return lane_lines_arr
 
@@ -185,13 +208,16 @@ def compute_steering_angle(frame, lane_lines_arr):
         **************** We assume that camera is calibrated to point to dead center ****************
     
     """
-    if len(lane_lines_arr) == 0:
-        logging.info('No lane lines detected')
-        return -90
 
+    if len(lane_lines_arr) == 0:
+        #logging.info('No lane lines detected')
+        return -90
+    
+    e1 = cv2.getTickCount()
     height, width, _ = frame.shape
+
     if len(lane_lines_arr) == 1:
-        logging.debug('Only detected one lane line, just follow it. %s' % lane_lines_arr[0])
+        #logging.debug('Only detected one lane line, just follow it. %s' % lane_lines_arr[0])
         x1, _, x2, _ = lane_lines_arr[0][0]
         x_offset = x2 - x1
     else:
@@ -208,7 +234,10 @@ def compute_steering_angle(frame, lane_lines_arr):
     angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
     steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
 
-    logging.debug('new steering angle: %s' % steering_angle)
+    #logging.debug('new steering angle: %s' % steering_angle)
+    e2 = cv2.getTickCount()
+    time = (e2 - e1)/ cv2.getTickFrequency()
+    print("compute_steering_angle :", time)
     return steering_angle
 
 def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lane_lines, max_angle_deviation_two_lines=5, max_angle_deviation_one_lane=1):
@@ -217,6 +246,9 @@ def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lan
     This can be improved to use last N angles, etc
     if new angle is too different from current angle, only turn by max_angle_deviation degrees
     """
+
+    e1 = cv2.getTickCount()
+
     if num_of_lane_lines == 2 :
         # if both lane lines detected, then we can deviate more
         max_angle_deviation = max_angle_deviation_two_lines
@@ -230,7 +262,12 @@ def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lan
                                         + max_angle_deviation * angle_deviation / abs(angle_deviation))
     else:
         stabilized_steering_angle = new_steering_angle
-    logging.info('Proposed angle: %s, stabilized angle: %s' % (new_steering_angle, stabilized_steering_angle))
+    #logging.info('Proposed angle: %s, stabilized angle: %s' % (new_steering_angle, stabilized_steering_angle))
+
+    e2 = cv2.getTickCount()
+    time = (e2 - e1)/ cv2.getTickFrequency()
+    print("stabilize_steering_angle :", time)
+
     return stabilized_steering_angle
 
 
@@ -245,7 +282,6 @@ def display_lines(frame, lines, line_color=(0, 255, 0), line_width=10):
                 cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
     line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     return line_image
-
 
 def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5, ):
     heading_image = np.zeros_like(frame)
@@ -279,6 +315,9 @@ def show_image(title, frame, show=_SHOW_IMAGE):
         cv2.imshow(title, frame)
 
 def make_points(frame, line):
+    
+    e1 = cv2.getTickCount()
+
     height, width, _ = frame.shape
     slope, intercept = line
     y1 = height  # bottom of the frame
@@ -287,6 +326,10 @@ def make_points(frame, line):
     # bound the coordinates within the frame
     x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
     x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+
+    e2 = cv2.getTickCount()
+    time = (e2 - e1)/ cv2.getTickFrequency()
+    print("make_points:", time)
     return [[x1, y1, x2, y2]]
 
 ############################
@@ -300,15 +343,6 @@ def test_photo(file):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-def getFPS(start, numFrames):
-    end = time.time()
-    elapsedSeconds = end - start
-    print("Time taken : {0} seconds".format(elapsedSeconds))
-    fps = numFrames/elapsedSeconds
-    print("Estimated FPS: {0}".format(fps))
-
-
 def test_video(video_file):
     lane_follower = HandCodedLaneFollower()
     # cap = cv2.VideoCapture(video_file + '.avi')
@@ -316,8 +350,8 @@ def test_video(video_file):
     
     # lower RESOLUTION for faster processing [https://picamera.readthedocs.io/en/release-1.12/fov.html]
     if _ENFORCE_RESOLUTION:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH,CAM_WIDTH);
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,CAM_HEIGHT);
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH,640);
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480);
 
     # skip first second of video.
     for i in range(3):
@@ -332,6 +366,17 @@ def test_video(video_file):
     try:
         i = 0
         while cap.isOpened():
+
+            # test on 'i' frames
+            if i==10:
+                end = time.time()
+                elapsedSeconds = end - start
+                # print FPS
+                print("Time taken : {0} seconds".format(elapsedSeconds))
+                fps = i/elapsedSeconds
+                print("Estimated FPS: {0}".format(fps))
+                break
+
             _, frame = cap.read()
             print('frame %s' % i )
             combo_image= lane_follower.follow_lane(frame)
@@ -341,13 +386,14 @@ def test_video(video_file):
             cv2.imshow("Road with Lane line", combo_image)
             i += 1
 
-            if i >= NUM_FRAMES:
-                getFPS(start, i)
-                break
-
             # press 'q' to quit
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 # print overall FPS
+                end = time.time()
+                elapsedSeconds = end - start
+                print("Time taken : {0} seconds".format(elapsedSeconds))
+                fps = i/elapsedSeconds
+                print("Estimated FPS: {0}".format(fps))
                 break
     finally:
         cap.release()
@@ -357,7 +403,10 @@ def test_video(video_file):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+
+    e1 = cv2.getTickCount()
+# your code execution
     test_video('/home/pi/DeepPiCar/driver/data/tmp/video01')
-    # test_photo('images/test.png')
-    #test_photo(sys.argv[1])
-    #test_video(sys.argv[1])
+    e2 = cv2.getTickCount()
+    time = (e2 - e1)/ cv2.getTickFrequency()
+    print("totaltime :", time)
